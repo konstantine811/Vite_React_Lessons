22e720e57319b7499f5664174482f9c9ec01c63b
konstantine811 <galystyan777@gmail.com>
2023-02-25 13:42:58 +0200

    create pet component jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
 import './App.scss'
+import Pet from './Pet';
 
 function App() {
 
   return (
-   <h1>Hello world</h1>
+   <div>
+    <h1>Hello world</h1>
+    <Pet name="Luna" animal="dog" breed="Havanse"></Pet>
+    <Pet name="Pepper" animal="bird" breed="Cockatiel"></Pet>
+    <Pet name="Doink" animal="cat" breed="Mixed"></Pet>
+   </div>
   )
 }
 
